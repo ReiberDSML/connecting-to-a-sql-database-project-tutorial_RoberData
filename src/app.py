a29b1dea195c9ab80ddb569735ed7d711ae8d6a1
Reiber DSML <robertomorell27@gmail.com>
2024-12-05 12:46:05 +0000

    First commit

--- src/app.py
+++ src/app.py
@@ -7,13 +7,14 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # 1) Connect to the database here using the SQLAlchemy's create_engine function
-
 connection_string = f"postgresql://{os.getenv('gitpod')}:{os.getenv('postgres')}@{os.getenv('localhost')}/{os.getenv('sample-db')}"
 engine = create_engine(connection_string).execution_options(autocommit=True)
 engine.connect()
 
-# 2) Execute the SQL sentences to create your tables using the SQLAlchemy's execute function
-engine.execute(""" CREATE TABLE publishers(
+
+
+# Creamos las tablas en nuestra database
+create_tables_sql = """ CREATE TABLE publishers(
     publisher_id INT NOT NULL,
     name VARCHAR(255) NOT NULL,
     PRIMARY KEY(publisher_id)
@@ -45,11 +46,15 @@ CREATE TABLE book_authors (
     PRIMARY KEY(book_id, author_id),
     CONSTRAINT fk_book FOREIGN KEY(book_id) REFERENCES books(book_id) ON DELETE CASCADE,
     CONSTRAINT fk_author FOREIGN KEY(author_id) REFERENCES authors(author_id) ON DELETE CASCADE
-);  """)
+);  """
+
+engine.execute(create_tables_sql)
 
 
-# 3) Execute the SQL sentences to insert your data using the SQLAlchemy's execute function
-engine.execute("""  -- publishers
+
+# Insertamos los datos del archivo insert.sql en la database
+
+insert_sql = """
 INSERT INTO publishers(publisher_id, name) VALUES (1, 'O Reilly Media');
 INSERT INTO publishers(publisher_id, name) VALUES (2, 'A Book Apart');
 INSERT INTO publishers(publisher_id, name) VALUES (3, 'A K PETERS');
@@ -58,7 +63,6 @@ INSERT INTO publishers(publisher_id, name) VALUES (5, 'Addison Wesley');
 INSERT INTO publishers(publisher_id, name) VALUES (6, 'Albert&Sweigart');
 INSERT INTO publishers(publisher_id, name) VALUES (7, 'Alfred A. Knopf');
 
--- authors 
 INSERT INTO authors (author_id, first_name, middle_name, last_name) VALUES (1, 'Merritt', null, 'Eric');
 INSERT INTO authors (author_id, first_name, middle_name, last_name) VALUES (2, 'Linda', null, 'Mui');
 INSERT INTO authors (author_id, first_name, middle_name, last_name) VALUES (3, 'Alecos', null, 'Papadatos');
@@ -68,7 +72,6 @@ INSERT INTO authors (author_id, first_name, middle_name, last_name) VALUES (6, '
 INSERT INTO authors (author_id, first_name, middle_name, last_name) VALUES (7, 'Yuval', 'Noah', 'Harari');
 INSERT INTO authors (author_id, first_name, middle_name, last_name) VALUES (8, 'Paul', null, 'Albitz');
 
--- books
 INSERT INTO books (book_id, title, total_pages, rating, isbn, published_date, publisher_id) VALUES (1, 'Lean Software Development: An Agile Toolkit', 240, 4.17, '9780320000000', '2003-05-18', 5);
 INSERT INTO books (book_id, title, total_pages, rating, isbn, published_date, publisher_id) VALUES (2, 'Facing the Intelligence Explosion', 91, 3.87, null, '2013-02-01', 7);
 INSERT INTO books (book_id, title, total_pages, rating, isbn, published_date, publisher_id) VALUES (3, 'Scala in Action', 419, 3.74, '9781940000000', '2013-04-10', 1);
@@ -80,7 +83,6 @@ INSERT INTO books (book_id, title, total_pages, rating, isbn, published_date, pu
 INSERT INTO books (book_id, title, total_pages, rating, isbn, published_date, publisher_id) VALUES (9, 'The Apollo Guidance Computer: Architecture And Operation (Springer Praxis Books / Space Exploration)', 439, 4.29, '9781440000000', '2010-07-01', 6);
 INSERT INTO books (book_id, title, total_pages, rating, isbn, published_date, publisher_id) VALUES (10, 'Minds and Computers: An Introduction to the Philosophy of Artificial Intelligence', 222, 3.54, '9780750000000', '2007-02-13', 7);
 
--- book authors
 INSERT INTO book_authors (book_id, author_id) VALUES (1, 1);
 INSERT INTO book_authors (book_id, author_id) VALUES (2, 8);
 INSERT INTO book_authors (book_id, author_id) VALUES (3, 7);
@@ -90,9 +92,11 @@ INSERT INTO book_authors (book_id, author_id) VALUES (6, 4);
 INSERT INTO book_authors (book_id, author_id) VALUES (7, 3);
 INSERT INTO book_authors (book_id, author_id) VALUES (8, 2);
 INSERT INTO book_authors (book_id, author_id) VALUES (9, 4);
-INSERT INTO book_authors (book_id, author_id) VALUES (10, 1);   """)
+INSERT INTO book_authors (book_id, author_id) VALUES (10, 1);   """
+
+engine.execute(insert_sql)
 
 
 # 4) Use pandas to print one of the tables as dataframes using read_sql function
-dataframe = pd.read_sql("Select * from books;", engine)
+dataframe = pd.read_sql("Select * from books_authors;", engine)
 print(dataframe.describe())
